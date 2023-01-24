5f9539557ecdbccd6e8f297b02b150c0791e5da8
ericmuh <muhwezi1000@gmail.com>
2023-01-24 23:15:00 +0300

    controllers, and model functions

--- app/Controllers/Poll.php
+++ app/Controllers/Poll.php
@@ -1,20 +1,32 @@
 <?php
 class Poll extends CI_Controller {
     public function index() {
-        // code to display poll question and options
-       
-            $this->load->model('Poll_model');
-            $data['poll_question'] = $this->Poll_model->get_poll_question();
-            $data['poll_options'] = $this->Poll_model->get_poll_options();
-            $this->load->view('poll_question_view', $data);
-        
+        // load the model
+        $this->load->model('Poll_model');
+        // get poll question and options
+        $data['poll_question'] = $this->Poll_model->get_poll_question();
+        $data['poll_options'] = $this->Poll_model->get_poll_options();
+        // load the view to display the question and options
+        $this->load->view('poll_question_view', $data);
     }
 
     public function vote() {
-        // code to handle vote submission
+        // get the option id from the form submission
+        $option_id = $this->input->post('option');
+        // load the model
+        $this->load->model('Poll_model');
+        // submit the vote
+        $this->Poll_model->submit_vote($option_id);
+        // redirect to the results page
+        redirect('poll/results');
     }
 
     public function results() {
-        // code to display poll results
+        // load the model
+        $this->load->model('Poll_model');
+        // get the results
+        $data['results'] = $this->Poll_model->get_results();
+        // load the view to display the results
+        $this->load->view('poll_results_view', $data);
     }
 }
--- app/Models/Poll_model.php
+++ app/Models/Poll_model.php
@@ -2,17 +2,30 @@
 class Poll_model extends CI_Model {
     public function get_poll_question() {
         // code to retrieve poll question from the database
+        $query = $this->db->get('polls');
+        return $query->row()->question;
     }
 
     public function get_poll_options() {
         // code to retrieve poll options from the database
+        $this->db->select('*');
+        $this->db->from('poll_options');
+        $query = $this->db->get();
+        return $query->result_array();
     }
 
     public function submit_vote($option_id) {
         // code to submit vote to the database
+        $data = array(
+            'option_id' => $option_id,
+            'created_at' => date('Y-m-d H:i:s')
+        );
+        $this->db->insert('poll_votes', $data);
     }
 
     public function get_results() {
         // code to retrieve poll results from the database
+        $query = $this->db->query('SELECT poll_options.name, COUNT(poll_votes.option_id) as votes, (COUNT(poll_votes.option_id) / (SELECT COUNT(*) FROM poll_votes)) * 100 as percentage FROM poll_options LEFT JOIN poll_votes ON poll_options.id = poll_votes.option_id GROUP BY poll_options.id');
+        return $query->result_array();
     }
 }
