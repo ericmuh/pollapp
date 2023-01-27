1cd86550a59b81224571c784d4952fd9580a623d
ericmuh <muhwezi1000@gmail.com>
2023-01-27 20:44:02 +0300

    poll selection

--- app/Views/home_view.php
+++ app/Views/home_view.php
@@ -9,3 +9,13 @@
         </li>
     <?php endforeach; ?>
 </ul>
+
+<!-- view for displaying poll question and options -->
+<h1><?php echo $polls; ?></h1>
+<form action="<?php echo base_url('poll/vote'); ?>" method="post">
+    <?php foreach ($polls as $option) : ?>
+        <input type="radio" name="option" value="<?php echo $option['id']; ?>">
+        <label><?php echo $option['question']; ?></label><br>
+    <?php endforeach; ?>
+    <input type="submit" value="Vote">
+</form>
